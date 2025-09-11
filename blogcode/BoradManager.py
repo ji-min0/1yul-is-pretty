2501ff58f433bd77d78df897b526a9338c5ad17b
choiguen <daindj0@gmail.com>
2025-09-11 14:40:26 +0900

    fix: 포메팅오류 수정

--- blogcode/BoradManager.py
+++ blogcode/BoradManager.py
@@ -294,28 +294,25 @@ class BoardManager:
 #!==========================================================================
 
 #?================================수정======================================
-    def select_and_view_post(posts):  # 게시글 목록을 불러오면 (전체글, 카테고리, 최신순 등등) 그 상태에서 번호를 입력해 글 내용을 불러오는 함수
-        pass
-        # if not posts:
-        #     print("표시할 게시글이 없습니다.")
-        #     return
-        # try:
-        #     choice = int(input("\n게시글 번호를 입력하세요 (0: 취소): "))
-        #     if choice == 0:
-        #         print("취소되었습니다.")
-        #         return
-        #     elif 1 <= choice <= len(posts):
-        #         print(posts)
-        #         selected_title, selected_filename = posts[choice-1]['title'], posts[choice-1]['id']
-        #         print(selected_title, selected_filename)
-        #         print(f"\n선택한 게시글: {selected_title}")
-        #         show_post_content(selected_filename)
-        #     else:
-        #         print("잘못된 번호입니다.")
-        # except ValueError:
-        #     print("숫자를 입력해주세요.")
-
-
+    def select_and_view_post(self, posts):  # 게시글 목록을 불러오면 (전체글, 카테고리, 최신순 등등) 그 상태에서 번호를 입력해 글 내용을 불러오는 함수
+        if not posts:
+            print("표시할 게시글이 없습니다.")
+            return
+        try:
+            choice = int(input("\n게시글 번호를 입력하세요 (0: 취소): "))
+            if choice == 0:
+                print("취소되었습니다.")
+                return
+            elif 1 <= choice <= len(posts):
+                print(posts)
+                selected_title, selected_filename = posts[choice-1]['title'], posts[choice-1]['id']
+                print(selected_title, selected_filename)
+                print(f"\n선택한 게시글: {selected_title}")
+                self.show_post_content(selected_filename)
+            else:
+                print("잘못된 번호입니다.")
+        except ValueError:
+            print("숫자를 입력해주세요.")
 #?==========================================================================
 
 #!================================원본=======================================
